cbcf732b5d7243e1e9d74d2ef9384d2a3bbb0b67
FahimHossain2186 <fahim.hossain.251@northsouth.edu>
2026-04-21 18:23:07 +0600

    added some changes

--- students-grade-system/students-grade-system/utility/database_print.py
+++ students-grade-system/students-grade-system/utility/database_print.py
@@ -6,18 +6,30 @@ The function prints the student ID, name, and either the test scores or the aver
 
 def database_print(student_id, student_info, format):
 
+    """
+    The Student ID consists of 5 digits, so using (9-len(student_id)) 
+    we calculate the number of spaces needed to align the student ID to the right.
+    The student name is left-aligned with a width of 20 characters.
+
+    """
+
     print(f"{' ' * (9-len(student_id))}{student_id}  {student_info['name']:<20}", end = " ")
 
     if format == "test":
+
         for score in student_info["grades"]:
-            print(f"{score:<5}", end = " ")
+            # Each score is formatted with 5 characters as the chracter count for (Test1, Test2, Test3) is 5 
+            print(f"{score:<5}", end = " ")         
+        
         print()
+    
     elif format == "avg":
+        
         count = 0
         total = 0
 
         if len(student_info["grades"]) == 0:
-            print(f"{'N/A':>8}")
+            print(f"{'N/A'}")
             return
 
         for score in student_info["grades"]:
