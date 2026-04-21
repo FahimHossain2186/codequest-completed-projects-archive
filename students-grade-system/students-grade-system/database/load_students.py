124ed5e156f53b38c9035309295ff398f8988520
FahimHossain2186 <fahim.hossain.251@northsouth.edu>
2026-04-21 12:55:34 +0600

    added documentation (docstrings and comments)

--- students-grade-system/students-grade-system/database/load_students.py
+++ students-grade-system/students-grade-system/database/load_students.py
@@ -14,7 +14,7 @@ The style we used for dictionary is:
 students = {
     "student_id1": {
         "name": "Student Name 1",
-        "grades": ["85.0", "90.0", "78.0"]          # List of grades as strings
+        "grades": ["grade1", "grade2", "grade3", ...]          # List of grades as strings
     }
 }
 '''
@@ -22,10 +22,66 @@ students = {
 def load_students(FILE_NAME):
 
     students = {}
-    FILE_NAME = os.path.join(os.path.dirname(__file__), "data", FILE_NAME)      # Get the absolute path to the file in the data directory
+
+    # Get the absolute path to the file in the data directory
+    
+    '''                                        
+    __file__ --> the path of the current file (load_students.py)
+    os.path.dirname(__file__) --> the directory of the current file (database)
+    os.path.join(os.path.dirname(__file__), "data", FILE_NAME) --> directory of the current file/data/"FILE_NAME" 
+
+    This ensures that the file is correctly located regardless of where the script is run from, 
+    as it constructs the path relative to the location of the current script.
+    '''
+
+    # Using raw string for docstrings to avoid SyntaxWarning: invalid escape sequence '\s'
+    r"""
+    print(__file__)
+    print(os.path.dirname(__file__))
+    print(os.path.join(os.path.dirname(__file__), "data", FILE_NAME))
+
+    The output will be something like this:
+    ...\students-grade-system\database\load_students.py
+    ...\students-grade-system\database
+    ...\students-grade-system\database\data\grades.txt
+    """
+    
+
+    FILE_NAME = os.path.join(os.path.dirname(__file__), "data", FILE_NAME)      
+
 
     try:
         with open(FILE_NAME, "r") as file:
+
+            """
+            Lets consider the first line of the file is:
+            91007# Ahmad Said# 50.0 78.5 73.2
+
+            line.strip() --> "91007# Ahmad Said# 50.0 78.5 73.2"
+            line.count("#") --> 2 (there are 2 '#' in the line)
+            line.split("#") --> ["91007", " Ahmad Said", " 50.0 78.5 73.2"]
+
+            student_id, student_name, student_grades = line.split("#") --> 
+            student_id = "91007", student_name = " Ahmad Said", student_grades = " 50.0 78.5 73.2"
+
+            students[student_id.strip()] --> students["91007"] = {
+                "name" : student_name.strip(), --> "Ahmad Said"
+                "grades" : student_grades.split()} --> ["50.0", "78.5", "73.2"]
+                }
+            
+            # After processing the first line, the students dictionary will look like this:
+            students = {
+                "91007": {
+                    "name": "Ahmad Said",
+                    "grades": ["50.0", "78.5", "73.2"]
+                }
+            }
+
+            Here 91007 is the key to initial dictionary and the value is another dictionary that is {"name": "Ahmad Said", "grades": ["50.0", "78.5", "73.2"]}
+            In the inner dictionary, "name" is the key and "Ahmad Said" is the value, 
+            and "grades" is the key and ["50.0", "78.5", "73.2"] is the value which is a list of strings.
+            """
+
             for line in file:
                 line = line.strip()
                 if not line or line.count("#") != 2:
@@ -35,7 +91,8 @@ def load_students(FILE_NAME):
 
                 students[student_id.strip()] = {
                     "name" : student_name.strip(),
-                    "grades" : student_grades.split()}
+                    "grades" : student_grades.split()
+                    }
 
         #print(students)
 
